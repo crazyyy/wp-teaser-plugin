d6733fef78d7e5e7832b817816eddffa240a6189
crazyyy <crazyyy@gmail.com>
2016-09-28 01:33:07 +0300

    robert de niro

--- assets/js/roshenka-front.js
+++ assets/js/roshenka-front.js
@@ -5,19 +5,19 @@ if (typeof jQuery === 'undefined') {
 }
 var $ = jQuery;
 
-$(window).scroll(function() {
-  var $widget = $(".roshenka");
-  var window_offset = $widget.offset().top - $(window).scrollTop();
-  if ( window_offset <= 0 ) {
-    $widget.css({
-      position: 'fixed',
-      top: '0'
-    });
-  } else {
-    $widget.css({
-      position: 'relative',
-      top: 'auto'
-    });
-  }
-  console.log(window_offset);
-});
+// $(window).scroll(function() {
+//   var $widget = $(".roshenka");
+//   var window_offset = $widget.offset().top - $(window).scrollTop();
+//   if ( window_offset <= 0 ) {
+//     $widget.css({
+//       position: 'fixed',
+//       top: '0'
+//     });
+//   } else {
+//     $widget.css({
+//       position: 'relative',
+//       top: 'auto'
+//     });
+//   }
+//   console.log(window_offset);
+// });
--- wordpress/wp-content/plugins/roshenka/js/roshenka-front.js
+++ wordpress/wp-content/plugins/roshenka/js/roshenka-front.js
@@ -1,2 +1,2 @@
-"undefined"==typeof jQuery?console.warn("jQuery hasn't loaded"):console.log("jQuery has loaded");var $=jQuery;$(window).scroll(function(){var o=$(".roshenka"),e=o.offset().top-$(window).scrollTop();e<=0?o.css({position:"fixed",top:"0"}):o.css({position:"relative",top:"auto"}),console.log(e)});
+"undefined"==typeof jQuery?console.warn("jQuery hasn't loaded"):console.log("jQuery has loaded");var $=jQuery;
 //# sourceMappingURL=maps/roshenka-front.js.map
